bbed3a82c5f74d4a1f5075ec1d7190fdb308977c
MARIO ALMAGUER <malmaguerv@gmail.com>
2014-08-06 05:43:06 -0400

    move
    changed simple move for MOVE, added gravity, added jumping

--- Tutorial1/Assets/Scripts/FirstPersonController.cs
+++ Tutorial1/Assets/Scripts/FirstPersonController.cs
@@ -1,23 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+[RequireComponent (typeof(CharacterController))]
 public class FirstPersonController : MonoBehaviour {
 
 	public float movementSpeed = 5.0f;
 	public float mouseSensitivity = 5.0f;
+	public float jumpSpeed = 20.0f;
 	float verticalRotation = 0;
 	public float upDownRange = 60.0f;
+
+	float verticalVelocity = 0;
 	
+	CharacterController characterController;
+		
 	// Use this for initialization
 	void Start () 
 	{
 	Screen.lockCursor = true;
+	characterController = GetComponent<CharacterController>();
+	
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-	
+
 		//ROTATION
 		
 		float rotLeftRight = Input.GetAxis ("Mouse X") * mouseSensitivity;
@@ -29,16 +36,22 @@ public class FirstPersonController : MonoBehaviour {
 		Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0,0);		
 		
 		//MOVEMENT
+		
+
 		float forwardSpeed = Input.GetAxis("Vertical") * movementSpeed;
 		float sideSpeed = Input.GetAxis("Horizontal") * movementSpeed;
 		
-		Vector3 speed = new Vector3 ( sideSpeed, 0, forwardSpeed );
+		verticalVelocity += Physics.gravity.y * Time.deltaTime;
+		
+		if(characterController.isGrounded && Input.GetButton("Jump")) {
+			verticalVelocity = jumpSpeed;
+		}
+		
+		Vector3 speed = new Vector3 ( sideSpeed, verticalVelocity, forwardSpeed );
 		
 		speed = transform.rotation * speed;
-		
-		CharacterController cc = GetComponent<CharacterController>();
-		
-		cc.SimpleMove( speed );
+				
+		characterController.Move( speed * Time.deltaTime);
 		
 		
 	}
