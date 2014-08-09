3327bf63d25febc9761c858e03948fceac330b04
MARIO ALMAGUER <malmaguerv@gmail.com>
2014-08-08 22:52:25 -0400

    bullet + bullet script start
    started doing the bullet spawning and destroy

---
+++ Tutorial1/Assets/Scripts/FP_Shooting.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FP_Shooting : MonoBehaviour {
+	
+	public GameObject bullet_prefab;
+	float bulletImpulse = 20f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if( Input.GetButtonDown ("Fire1")) {
+		Camera cam = Camera.main;
+		GameObject thebullet = (GameObject)Instantiate(bullet_prefab, cam.transform.position + cam.transform.forward, cam.transform.rotation);			
+		thebullet.rigidbody.AddForce(cam.transform.forward * bulletImpulse,ForceMode.Impulse);
+		}
+	}
+}
---
+++ Tutorial1/Assets/Scripts/bullet_thermaldetonator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bullet_thermaldetonator : MonoBehaviour {
+	
+	float lifespan = 3.0f;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lifespan -= Time.deltaTime;
+		if(lifespan<= 0) {
+				Explode();
+		}
+	}
+	
+	void OnCollisionEnter() {
+		//Destroy(gameObject);
+	}
+		
+	void Explode() {
+			
+			Destroy (gameObject);
+	
+	}
+}
